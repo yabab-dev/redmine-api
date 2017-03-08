d0ca353fd9491fcb351d468ba0c827b12ed3524a
Simon Babay <simon@babay.fr>
2017-03-08 22:46:22 +0100

    Update samples

--- samples/node.js
+++ samples/node.js
@@ -1,9 +1,9 @@
-// Take functions from node-fetch and set globally
+// Take functions from node-fetch and set globaly
 global.fetch = require('node-fetch');
 global.Headers = require('node-fetch/lib/headers');
 
 // Load lib
-const { RedmineAPI } = require('@chymz/redmine-api');
+const { RedmineAPI } = require('../dist/redmine-api');
 
 // Init Redmine API client
 let api = new RedmineAPI('https://www.redmine.org');
